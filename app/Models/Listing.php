43ddc4ccf80bfb5655d89b3356bbb1f61588997c
Kate <murraykeaton422@gmail.com>
2023-11-02 02:22:46 -0400

    created listing model using artisan

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,33 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-
-    public static function all() {
-        return [
-
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'This is a dummy description for listing one'
-                ],
-
-                [
-                    'id' => 2,
-                    'title' => 'Listing Two',
-                    'description' => 'This is a dummy description for listing two'
-                ]
-        ];
-    }
-
-    public static function find($id) {
-        $listings = self::all();
-
-        foreach($listings as $listing) {
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
