f1718b6a66f408ebd295ffebe3fba1852e8efe10
Kate <murraykeaton422@gmail.com>
2023-11-02 03:05:34 -0400

    seeded dummy datas from factory into db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
          \App\Models\User::factory(10)->create();
 
+         Listing::factory(6)->create();
+        /*
          Listing::create(
             [
 
@@ -45,5 +47,6 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        */
     }
 }
