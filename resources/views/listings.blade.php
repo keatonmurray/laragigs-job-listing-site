6e8a1b17caf1b537e28382fa44993cee048e3950
Kate <murraykeaton422@gmail.com>
2023-11-02 04:09:16 -0400

    added a dynamic search bar to landing page

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 @section('content')
 @include('partials._hero')
+@include('partials._search')
 <div class="bg-gray-50 border border-gray-200 rounded p-6"> 
    @if(count($listings) == 0)
       <p>No listings found</p>
