cbed5e8fb86504f0aa90633499ebb00693c2ce8e
Kate <murraykeaton422@gmail.com>
2023-11-11 02:46:32 -0500

    modified textarea value as it should not have a value

--- resources/views/listings/create.blade.php
+++ resources/views/listings/create.blade.php
@@ -63,7 +63,7 @@
         -->
         <div class="mb-6">
             <label for="description" class="inline-block text-lg mb-2"> Job Description</label>
-            <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="Include tasks, requirements, salary, etc" value="{{old('description')}}"></textarea>
+            <textarea class="border border-gray-200 rounded p-2 w-full" name="description" rows="10" placeholder="Include tasks, requirements, salary, etc">{{old('description')}}</textarea>
             @error('description')
                 <p class="text-red-500 text-xs">{{$message}}</p>
             @enderror
