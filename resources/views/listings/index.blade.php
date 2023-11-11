ee8523bfdf9648a077b469056d32699b36247dd9
Kate <murraykeaton422@gmail.com>
2023-11-11 03:13:54 -0500

    modified content divs to display 2 columns in the front page in bigger screen dimensions

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 @include('partials._hero')
 @include('partials._search')
-<div class="bg-gray-50 border border-gray-200 rounded p-6"> 
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4"> 
    @if(count($listings) == 0)
       <p>No listings found</p>
    @endif
